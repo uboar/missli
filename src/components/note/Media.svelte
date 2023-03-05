996c3fc98c4427db7ae39256db9f2db5a7e19644
uboar <uboaruboar@gmail.com>
2023-03-06 08:54:15 +0900

    ファイル表示を追加

--- src/components/note/Media.svelte
+++ src/components/note/Media.svelte
@@ -28,6 +28,20 @@
         </video>
       {:else if file.type.indexOf("audio") >= 0}
         <audio controls src={file.url} />
+      {:else}
+        <a href={file.url} class="alert w-full">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="h-6 w-6"
+              ><path
+                d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
+              /></svg
+            >
+            <span class="link link-hover">{file.name}</span>
+          </div>
+        </a>
       {/if}
     {/each}
   {/if}
