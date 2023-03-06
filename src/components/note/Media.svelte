084425963dacae15888e20150d5d50b9b363865c
uboar <uboaruboar@gmail.com>
2023-03-06 09:03:00 +0900

    メディアアイテムに高さ制限を追加

--- src/components/note/Media.svelte
+++ src/components/note/Media.svelte
@@ -5,44 +5,49 @@
 </script>
 
 <!-- メディア内容 -->
-<div class="carousel justify-center">
+<div class="carousel justify-center max-h-64">
   {#if files.length > 0}
-    {#each files as file (file.id)}
-      {#if file.type.indexOf("image") >= 0}
-        <a
-          class="carousel-item rounded-lg my-2"
-          href={file.url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={file.thumbnailUrl}
-            alt={file.name}
-            class="rounded-lg {file.isSensitive ? 'blur' : ''}"
-          />
-        </a>
-      {:else if file.type.indexOf("video") >= 0}
-        <video controls>
-          <source src={file.url} />
-          <track kind="captions" />
-        </video>
-      {:else if file.type.indexOf("audio") >= 0}
-        <audio controls src={file.url} />
-      {:else}
-        <a href={file.url} class="alert w-full">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="h-6 w-6"
-              ><path
-                d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
-              /></svg
-            >
-            <span class="link link-hover">{file.name}</span>
-          </div>
-        </a>
-      {/if}
+    {#each files as file, index (file.id)}
+        {#if file.type.indexOf("image") >= 0}
+          <a
+            class="carousel-item rounded-lg my-2"
+            href={file.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={file.thumbnailUrl}
+              alt={file.name}
+              class="object-contain rounded-lg {file.isSensitive ? 'blur' : ''}"
+            />
+          </a>
+        {:else if file.type.indexOf("video") >= 0}
+          <video controls>
+            <source src={file.url} />
+            <track kind="captions" />
+          </video>
+        {:else if file.type.indexOf("audio") >= 0}
+          <audio controls src={file.url} />
+        {:else}
+          <a
+            href={file.url}
+            target="_blank"
+            rel="noreferrer"
+            class="alert w-full link link-hover"
+          >
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                class="h-6 w-6"
+                ><path
+                  d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
+                /></svg
+              >
+              <span>{file.name}</span>
+            </div>
+          </a>
+        {/if}
     {/each}
   {/if}
 </div>
