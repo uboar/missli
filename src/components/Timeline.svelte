b2b8703e5f179423f6a4dd78d9af862328df223c
uboar <uboaruboar@gmail.com>
2023-03-06 20:24:57 +0900

    タイムラインを畳む機能を追加

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -32,6 +32,7 @@
     bufferNoteNum: 250,
     initialNotes: [],
     reactionDeck: [],
+    isCollapsed: false,
   };
 
   const NAV = {
@@ -238,164 +239,187 @@
   });
 </script>
 
-<div class="h-full bg-base-300 relative rounded" style="width:{options.width}">
-  <div class="absolute w-full flex justify-center z-10">
-    <div class="flex flex-col w-full mx-8">
+{#if !options.isCollapsed}
+  <div
+    class="h-full bg-base-300 relative rounded"
+    style="width:{options.width}"
+  >
+    <div class="absolute w-full flex justify-center z-10">
+      <div class="flex flex-col w-full ml-8">
+        <button
+          class="btn btn-xs bg-base-100 btn-outline btn-block my-1 normal-case"
+          style="color: {options.color}"
+          on:click={() => (scrollPos.scrollTop = 0)}
+        >
+          {options.channelName}
+        </button>
+      </div>
       <button
-        class="btn btn-xs bg-base-100 btn-outline btn-block my-1"
+        class="btn btn-xs bg-base-100 my-1 mr-8 ml-2 btn-outline btn-square"
         style="color: {options.color}"
-        on:click={() => (scrollPos.scrollTop = 0)}
+        on:click={() => (options.isCollapsed = true)}
       >
-        {options.channelName}
+        ❮
       </button>
     </div>
-  </div>
-  <div
-    class="w-full timeline-body absolute pt-8 h-full overflow-y-scroll z-0 overflow-x-hidden inline-flex overscroll-none"
-    bind:this={scrollPos}
-    style:--color={options.color + "20"}
-  >
-    {#if !errFlg}
-      <!-- オプション -->
-      {#if showNav === NAV.settings}
-        <TimelineOptions
-          bind:options
-          on:deleteRequest={() => timelineDelete()}
-          on:getNoteRequest={() => console.log(notes)}
-        />
-      {:else}
-        <!-- ノート表示 -->
-        <div class="relative w-full z-10">
-          {#each showNotes as note (note.id)}
-            <Note
-              {note}
-              {user}
-              timelineOptions={options}
-              stream={user.stream}
-              on:renoteRequest={() => renoteRequest(note)}
-              on:replyRequest={() => replyRequest(note)}
-            />
-          {/each}
-          <button
-            class="btn btn-block btn-secondary my-2"
-            on:click={() => {
-              scrollPos.scrollTop = 0;
-              beginNotes = 0;
-            }}>最初に戻る</button
-          >
-          {#if notes.length > options.showNoteNum && notes.length - options.showNoteNum > beginNotes}
+    <div
+      class="w-full timeline-body absolute pt-8 h-full overflow-y-scroll z-0 overflow-x-hidden inline-flex overscroll-none"
+      bind:this={scrollPos}
+      style:--color={options.color + "20"}
+    >
+      {#if !errFlg}
+        <!-- オプション -->
+        {#if showNav === NAV.settings}
+          <TimelineOptions
+            bind:options
+            on:deleteRequest={() => timelineDelete()}
+            on:getNoteRequest={() => console.log(notes)}
+          />
+        {:else}
+          <!-- ノート表示 -->
+          <div class="relative w-full z-10">
+            {#each showNotes as note (note.id)}
+              <Note
+                {note}
+                {user}
+                timelineOptions={options}
+                stream={user.stream}
+                on:renoteRequest={() => renoteRequest(note)}
+                on:replyRequest={() => replyRequest(note)}
+              />
+            {/each}
             <button
-              class="btn btn-block btn-primary my-2"
+              class="btn btn-block btn-secondary my-2"
               on:click={() => {
-                beginNotes += options.showNoteNum / 2;
-              }}>もっと表示</button
+                scrollPos.scrollTop = 0;
+                beginNotes = 0;
+              }}>最初に戻る</button
             >
-          {:else}
-            <button class="btn btn-block btn-primary my-2" on:click={moreNote}
-              >もっと表示(追加読み込み)</button
-            >{/if}
-          <div class="pt-16" />
+            {#if notes.length > options.showNoteNum && notes.length - options.showNoteNum > beginNotes}
+              <button
+                class="btn btn-block btn-primary my-2"
+                on:click={() => {
+                  beginNotes += options.showNoteNum / 2;
+                }}>もっと表示</button
+              >
+            {:else}
+              <button class="btn btn-block btn-primary my-2" on:click={moreNote}
+                >もっと表示(追加読み込み)</button
+              >{/if}
+            <div class="pt-16" />
+          </div>
+        {/if}
+      {:else}
+        <div class="alert alert-error h-10">
+          ホストとの通信にエラーが発生しました
         </div>
       {/if}
-    {:else}
-      <div class="alert alert-error h-10">
-        ホストとの通信にエラーが発生しました
-      </div>
-    {/if}
-  </div>
-  <div
-    class="absolute bottom-0 w-full z-50 bg-base-200 bg-opacity-90 border-b-4"
-    style="border-color:{options.color}"
-  >
-    <!-- タイムラインのフッター -->
-    {#if showNav === NAV.note}
-      <TimelinePostNote
-        bind:user
-        bind:postNote
-        bind:replyNote
-        bind:renoteNote
-        on:breakRequest={() => (showNav = NAV.none)}
-      />
-    {/if}
-    {#if showNav === NAV.notify}
-      <TimelineNotify bind:user />
-    {/if}
-    <div class="flex my-1 justify-around">
-      <!-- 通知ボタン -->
-      <div
-        class="tooltip"
-        data-tip={showNav === NAV.notify ? "閉じる" : "通知"}
-      >
-        <div class="indicator">
-          {#if user.notifyUnOpen}
-            <span class="indicator-item badge badge-secondary" />
-          {/if}
+    </div>
+    <div
+      class="absolute bottom-0 w-full z-50 bg-base-200 bg-opacity-90 border-b-4"
+      style="border-color:{options.color}"
+    >
+      <!-- タイムラインのフッター -->
+      {#if showNav === NAV.note}
+        <TimelinePostNote
+          bind:user
+          bind:postNote
+          bind:replyNote
+          bind:renoteNote
+          on:breakRequest={() => (showNav = NAV.none)}
+        />
+      {/if}
+      {#if showNav === NAV.notify}
+        <TimelineNotify bind:user />
+      {/if}
+      <div class="flex my-1 justify-around">
+        <!-- 通知ボタン -->
+        <div
+          class="tooltip"
+          data-tip={showNav === NAV.notify ? "閉じる" : "通知"}
+        >
+          <div class="indicator">
+            {#if user.notifyUnOpen}
+              <span class="indicator-item badge badge-secondary" />
+            {/if}
+            <button
+              class="btn btn-circle btn-outline fill-base-content hover:fill-base-100"
+              on:click={() =>
+                (showNav = showNav === NAV.notify ? NAV.none : NAV.notify)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                class="h-8 w-8"
+                ><path
+                  d={showNav === NAV.notify
+                    ? "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                    : "M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M14,21A2,2 0 0,1 12,23A2,2 0 0,1 10,21"}
+                /></svg
+              >
+            </button>
+          </div>
+        </div>
+        <!-- ノートボタン -->
+        <div
+          class="tooltip"
+          data-tip={showNav === NAV.note ? "閉じる" : "ノート"}
+        >
           <button
-            class="btn btn-circle btn-outline fill-base-content hover:fill-base-100"
+            class="btn btn-circle btn-primary fill-base-100"
             on:click={() =>
-              (showNav = showNav === NAV.notify ? NAV.none : NAV.notify)}
+              (showNav = showNav === NAV.note ? NAV.none : NAV.note)}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               class="h-8 w-8"
               ><path
-                d={showNav === NAV.notify
+                d={showNav === NAV.note
                   ? "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-                  : "M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M14,21A2,2 0 0,1 12,23A2,2 0 0,1 10,21"}
+                  : "M2,21L23,12L2,3V10L17,12L2,14V21Z"}
+              /></svg
+            >
+          </button>
+        </div>
+
+        <!-- 設定ボタン -->
+        <div
+          class="tooltip"
+          data-tip={showNav === NAV.settings ? "閉じる" : "タイムラインの設定"}
+        >
+          <button
+            class="btn btn-circle btn-outline fill-base-content hover:fill-base-100"
+            on:click={() =>
+              (showNav = showNav === NAV.settings ? NAV.none : NAV.settings)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="h-8 w-8"
+              ><path
+                d={showNav === NAV.settings
+                  ? "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                  : "M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z"}
               /></svg
             >
           </button>
         </div>
       </div>
-      <!-- ノートボタン -->
-      <div
-        class="tooltip"
-        data-tip={showNav === NAV.note ? "閉じる" : "ノート"}
-      >
-        <button
-          class="btn btn-circle btn-primary fill-base-100"
-          on:click={() =>
-            (showNav = showNav === NAV.note ? NAV.none : NAV.note)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="h-8 w-8"
-            ><path
-              d={showNav === NAV.note
-                ? "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-                : "M2,21L23,12L2,3V10L17,12L2,14V21Z"}
-            /></svg
-          >
-        </button>
-      </div>
-
-      <!-- 設定ボタン -->
-      <div
-        class="tooltip"
-        data-tip={showNav === NAV.settings ? "閉じる" : "タイムラインの設定"}
-      >
-        <button
-          class="btn btn-circle btn-outline fill-base-content hover:fill-base-100"
-          on:click={() =>
-            (showNav = showNav === NAV.settings ? NAV.none : NAV.settings)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="h-8 w-8"
-            ><path
-              d={showNav === NAV.settings
-                ? "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-                : "M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z"}
-            /></svg
-          >
-        </button>
-      </div>
     </div>
   </div>
-</div>
+{:else}
+  <div
+    class="h-full bg-base-300 relative rounded w-8 grid place-content-center"
+  >
+    <button
+      class="badge badge-outline link -rotate-90 link-hover animate-none"
+      style="color: {options.color}; width: 70vh"
+      on:click={() => (options.isCollapsed = false)}
+      >{options.channelName}</button
+    >
+  </div>
+{/if}
 
 <style>
   div::-webkit-scrollbar-track {
