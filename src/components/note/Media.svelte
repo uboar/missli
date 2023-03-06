0a59b15daa9354fec49fcabd18718aac64e5bad7
uboar <uboaruboar@gmail.com>
2023-03-06 09:43:27 +0900

    複数ファイルをボタンで切り替えるように変更、センシティブ画像を直接表示できるようにボタンを追加

--- src/components/note/Media.svelte
+++ src/components/note/Media.svelte
@@ -1,53 +1,81 @@
 <script lang="ts">
-  import type { Note } from "misskey-js/built/entities";
+  import type { DriveFile, Note } from "misskey-js/built/entities";
+  import { onMount } from "svelte";
 
   export let files: Note["files"];
+
+  $: file = files[index];
+  let index = 0;
+
+  const nextFile = () => {
+    if (index >= files.length - 1) index = 0;
+    else index++;
+  };
+  const previousFile = () => {
+    if (index === 0) index = files.length - 1;
+    else index--;
+  };
 </script>
 
 <!-- メディア内容 -->
-<div class="carousel justify-center max-h-64">
+<div class="w-full">
   {#if files.length > 0}
-    {#each files as file, index (file.id)}
-        {#if file.type.indexOf("image") >= 0}
-          <a
-            class="carousel-item rounded-lg my-2"
-            href={file.url}
-            target="_blank"
-            rel="noreferrer"
+    <div class="relative w-full flex justify-center max-h-64">
+      {#if file.type.indexOf("image") >= 0}
+        <a href={file.url} target="_blank" rel="noreferrer" class="relative">
+          <img
+            src={file.thumbnailUrl}
+            alt={file.name}
+            class="object-contain rounded-lg max-h-64 {file.isSensitive ? 'blur-lg' : ''}"
+          />
+        </a>
+        {#if file.isSensitive}
+          <div
+            class="absolute w-full h-full top-0 grid content-center justify-center z-20"
           >
-            <img
-              src={file.thumbnailUrl}
-              alt={file.name}
-              class="object-contain rounded-lg {file.isSensitive ? 'blur' : ''}"
-            />
-          </a>
-        {:else if file.type.indexOf("video") >= 0}
-          <video controls>
-            <source src={file.url} />
-            <track kind="captions" />
-          </video>
-        {:else if file.type.indexOf("audio") >= 0}
-          <audio controls src={file.url} />
-        {:else}
-          <a
-            href={file.url}
-            target="_blank"
-            rel="noreferrer"
-            class="alert w-full link link-hover"
-          >
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                class="h-6 w-6"
-                ><path
-                  d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
-                /></svg
-              >
-              <span>{file.name}</span>
-            </div>
-          </a>
+            <button class="btn w-32 btn-warning shadow-md" on:click={() => file.isSensitive = false}>画像を見る</button>
+          </div>
         {/if}
-    {/each}
+      {:else if file.type.indexOf("video") >= 0}
+        <video controls>
+          <source src={file.url} />
+          <track kind="captions" />
+        </video>
+      {:else if file.type.indexOf("audio") >= 0}
+        <audio controls src={file.url} />
+      {:else}
+        <a
+          href={file.url}
+          target="_blank"
+          rel="noreferrer"
+          class="alert w-full link link-hover"
+        >
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="h-6 w-6"
+              ><path
+                d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
+              /></svg
+            >
+            <span>{file.name}</span>
+          </div>
+        </a>
+      {/if}
+      {#if files.length > 1}
+        <div
+          class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+        >
+          <button
+            class="btn btn-circle btn-sm opacity-70"
+            on:click={previousFile}>❮</button
+          >
+          <button class="btn btn-circle btn-sm opacity-70" on:click={nextFile}
+            >❯</button
+          >
+        </div>
+      {/if}
+    </div>
   {/if}
 </div>
