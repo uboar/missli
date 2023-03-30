ad30491479af1765f8d576d0492f4ca96bd70a6c
uboar <uboaruboar@gmail.com>
2023-03-30 22:08:48 +0900

    CWを開かなくてもチャンネル名が表示されるように変更

--- src/components/note/Body.svelte
+++ src/components/note/Body.svelte
@@ -163,20 +163,20 @@
     </a>
   {/if}
 
-  {#if note.channelId}
-    <a
-      class="text-xs link fill-base-content text-base-content opacity-40"
-      href={`https://${user.hostUrl}/channels/${note.channelId}`}
-      target="_blank"
-      rel="noreferrer"
-      >{note.channel.name}
-    </a>
-  {/if}
-
   <!-- メディア内容 -->
   {#if note.files.length > 0 && !compact && !collapse}
     <Media files={note.files} {option} />
   {/if}
+{/if}
+
+{#if note.channelId}
+  <a
+    class="text-xs link fill-base-content text-base-content opacity-40"
+    href={`https://${user.hostUrl}/channels/${note.channelId}`}
+    target="_blank"
+    rel="noreferrer"
+    >{note.channel.name}
+  </a>
 {/if}
 
 {#if !compact}
